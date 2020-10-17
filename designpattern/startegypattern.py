8e1e426ede10c159512f8a4c2034a29fd6a23087
Gopi Krishna D <gopikrishnad2306@gmail.com>
2020-10-17 12:14:08 +0530

    strategy pattern

---
+++ designpattern/startegypattern.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+import typing
+from typing import Sequence, Optional, Callable
+
+
+class Customer(typing.NamedTuple):
+    name: str
+    fidelity: int
+
+
+class LineItem:
+    def __init__(self, product: str, quantity: int, price: float):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order:
+    """Order take cart of items and promotion to be applied
+    """
+    def __init__(self, customer: Customer, cart: Sequence[LineItem], promotion: Optional['Promotion'] = None):
+        self.customer = customer
+        self.cart = cart
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0.0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+class Promotion(ABC):  # the Strategy: an abstract base class
+    @abstractmethod
+    def discount(self, order: Order) -> float:
+        """Return discount on order"""
+        pass
+
+
+class FidelityPromo(Promotion):  # first Concrete Strategy
+    """5% discount for customers with 1000 or more fidelity points"""
+    def discount(self, order: Order) -> float:
+        return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
+
+class BulkItemPromo(Promotion):  # second Concrete Strategy
+    """10% discount for each LineItem with 20 or more units"""
+    def discount(self, order: Order) -> float:
+        discount = 0
+        for item in order.cart:
+            if item.quantity >= 20:
+                discount += item.total() * 0.10
+        return discount
+
+
+class LargeOrderPromo(Promotion):  # third Concrete strategy
+    """7% discount for orders with 10 or more distinct items"""
+    def discount(self, order: Order) -> float:
+        distinct_items = {item.product for item in order.cart}
+        if len(distinct_items) >= 10:
+            return order.total() * 0.7
+        return 0
+
+
+if __name__ == "__main__":
+    joe = Customer('John Doe',  0)
+    ann = Customer('Ann Smith',  1100)
+    cart = [LineItem('banana', 4, .5), LineItem('apple', 10, 1.5), LineItem('watermellon', 5, 5.0),
+            LineItem('mango', 5, 6.0), LineItem('jack', 5, 5.5), LineItem('biki', 5, 1.5),
+            LineItem('jiki', 5, 7.0), LineItem('tiki', 5, 8.0), LineItem('liki', 5, 6.0),
+            LineItem('greenapple', 5, 9.0)]
+    print(Order(joe, cart, FidelityPromo()))
+    print(Order(ann, cart, FidelityPromo()))
---
+++ trade.py
@@ -0,0 +1,90 @@
+"""
+Input:
+8
+2017-01-03,16:18:50,AAPL,142.64
+2017-01-03,16:25:22,AMD,13.86
+2017-01-03,16:25:25,AAPL,141.64
+2017-01-03,16:25:28,AMZN,845.61
+2017-01-03,16:28:50,AAPL,140.64
+2017-01-03,16:29:59,FB,140.34
+2017-01-04,16:29:32,AAPL,143.64
+2017-01-04,16:30:50,AAPL,141.64
+
+Output:
+Trading Day = 2017-01-03
+Last Quote Time = 16:29:59
+Number of valid quotes = 6
+Most active hour = 16
+Most active symbol = AAPL
+2017-01-03 16:28:50,AAPL,142.64,140.64
+2017-01-03 16:25:22,AMD,13.86,13.86
+2017-01-03 16:25:28,AMZN,845.61,845.61
+2017-01-03 16:29:59,FB,140.34,140.34
+Trading Day = 2017-01-04
+Last Quote Time = 16:29:32
+Number of valid quotes = 1
+Most active hour = 16
+Most active symbol = AAPL
+2017-01-04 16:29:32,AAPL,143.64,143.64
+"""
+import itertools
+import statistics
+
+
+class DayTrade(object):
+    """Trades specific to a date"""
+    def __init__(self, trading_date, day_trades):
+        self.trading_date = trading_date
+        self.day_trades = [trade for trade in day_trades]
+        self.last_quote = self.day_trades[-1][1]
+        self.valid_quotes = len(self.day_trades)
+        self.most_active_hour = statistics.mode([trade[1].split(':')[0] for trade in self.day_trades])
+        self.most_active_symbol = statistics.mode([trade[2] for trade in self.day_trades])
+
+    def report(self):
+        day_trades = sorted(self.day_trades, key=lambda x: x[2])
+        group_by_symbol = itertools.groupby(day_trades, lambda x: x[2])
+        print('Trading Day = {0}'.format(self.trading_date))
+        print('Last Quote Time = {0}'.format(self.last_quote))
+        print('Number of valid quotes = {0}'.format(self.valid_quotes))
+        print('Most active hour = {0}'.format(self.most_active_hour))
+        print('Most active symbol = {0}'.format(self.most_active_symbol))
+        for group in group_by_symbol:
+            symbol, trades = group
+            trades = [trade for trade in trades]
+            last_record = trades[-1]
+            zip_data = [trade for trade in zip(*trades)][3]
+            print(last_record[0], last_record[1], "{0},{1},{2}".format(symbol, max(zip_data), min(zip_data)))
+
+
+class TradeData(object):
+    """"Trades received from i/p with span of multiple days"""
+    def __init__(self, trades):
+        self.trades = trades
+        self.exchange_start_time = tuple(int(i) for i in '09:30:00'.split(':'))
+        self.exchange_end_time = tuple(int(i) for i in '16:30:00'.split(':'))
+
+    def divide_trades_by_session(self):
+        """filter out trades which are not valid and create a group based on day trade"""
+        valid_trades = filter(
+            lambda trade: self.exchange_start_time <= tuple(
+                int(i) for i in trade[1].split(':')) <= self.exchange_end_time,
+            self.trades)
+        trades_by_session = itertools.groupby(valid_trades, lambda x: x[0])
+        return trades_by_session
+
+    def gen_report(self):
+        """Final report"""
+        trades_by_day = self.divide_trades_by_session()
+        for day in trades_by_day:
+            DayTrade(*day).report()
+
+
+if __name__ == "__main__":
+    n = int(input())
+    ar = []
+    for _ in range(n):
+        t = input().split(',')
+        ar.append(t)
+    tr = TradeData(ar)
+    tr.gen_report()
